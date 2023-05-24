bb35da24c8c1c8497acec498a94f81d5fb2265e6
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-05-24 15:09:27 +0200

    fatto un mio bonus

--- js/index.js
+++ js/index.js
@@ -9,13 +9,23 @@ let list = [
 ]
 
 const stampa = document.getElementById('list');
+let myButton = document.getElementById("my-button");
+let inputText = document.getElementById("input-text");
+
 let i = 0;
 
 //Ciclo while
 while(i < list.length){
-    let lista = list[i]
-    let list_stampa = document.createElement('li')
-    stampa.append(list_stampa)
-    list_stampa.innerHTML = lista
+    let lista = list[i];
+    let list_stampa = document.createElement('li');
+    stampa.append(list_stampa);
+    list_stampa.innerHTML = lista;
     i++; 
 }
+
+myButton.addEventListener('click', function(){
+    let testo = inputText.value;
+    let list_stampa = document.createElement('li');
+    stampa.append(list_stampa);
+    list_stampa.innerHTML = testo;
+})
